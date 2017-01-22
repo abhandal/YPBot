beecbe88d75cd1eef0ddcfab3100a5a12fd2029c
Aman Bhandal <aman_bhandal@live.ca>
2017-01-22 01:48:30 -0500

    Added API call, has issues with performing the call

--- Backend/Java-API/YPBot/src/WebDriver.java
+++ Backend/Java-API/YPBot/src/WebDriver.java
@@ -3,7 +3,8 @@ public class WebDriver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		NlpApi go = new NlpApi("I want a hamburger so badly");
+		System.out.print(go.KeyPhrase());
 	}
 
 }
